1722c2ca50112609fd1993371ef0fbd0d5d649cd
[Eugene Joh] <ejoh@bu.edu>
2019-04-26 15:48:22 -0400

    created final slide deck for 1st presentation

--- markdown/00_render.R
+++ markdown/00_render.R
@@ -3,13 +3,17 @@
 
 library(here)
 library(rmarkdown)
-
+memes <- TRUE
 # render markdown presentation
-rmarkdown::render(input = here::here("markdown", "first_meeting_20190502.Rmd"), params = list(memes=FALSE, ani=FALSE))
+rmarkdown::render(input = here::here("markdown", "first_meeting_20190502.Rmd"), 
+                  params = list(memes=TRUE, ani=FALSE))
 
 # move file to slides
-file.copy(
-  from = list.files(here::here("markdown"), pattern = "20190502.*\\.html$", full.names = TRUE),
-  to = here::here("docs", "slides"),
-  overwrite = TRUE
-          )
+if (!memes) {
+  file.copy(
+    from = list.files(here::here("markdown"), pattern = "20190502.*\\.html$", full.names = TRUE),
+    to = here::here("docs", "slides"),
+    overwrite = TRUE
+  )
+}
+
