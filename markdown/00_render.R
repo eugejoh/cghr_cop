6cf5508f7f0fdf56fd7281ef1dfda7f70482d5a3
[Eugene Joh] <ejoh@bu.edu>
2019-04-26 15:46:23 -0400

    added css to resources website, un-memed the slides

--- markdown/00_render.R
+++ markdown/00_render.R
@@ -4,9 +4,12 @@
 library(here)
 library(rmarkdown)
 
-rmarkdown::render(input = here::here("markdown", "first_meeting_20190502.Rmd"))
+# render markdown presentation
+rmarkdown::render(input = here::here("markdown", "first_meeting_20190502.Rmd"), params = list(memes=FALSE, ani=FALSE))
 
+# move file to slides
 file.copy(
   from = list.files(here::here("markdown"), pattern = "20190502.*\\.html$", full.names = TRUE),
-  to = here::here("slides")
+  to = here::here("docs", "slides"),
+  overwrite = TRUE
           )
